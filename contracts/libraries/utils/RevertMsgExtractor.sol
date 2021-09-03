c5a06e3faa6104a47697e7f5fb301bda65994fb2
Tony Jin <nijynot@gmail.com>
2021-09-03 17:41:41 +0200

    fix new line formatting

--- contracts/libraries/utils/RevertMsgExtractor.sol
+++ contracts/libraries/utils/RevertMsgExtractor.sol
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: MIT
 // Taken from https://github.com/sushiswap/BoringSolidity/blob/441e51c0544cf2451e6116fe00515e71d7c42e2c/contracts/BoringBatchable.sol
 pragma solidity >=0.6.0;
