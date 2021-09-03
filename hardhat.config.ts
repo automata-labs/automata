7617d73be3eaae7984216b3557fffdb9e48e6ba2
Tony Jin <nijynot@gmail.com>
2021-09-03 13:16:47 +0200

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat'
+import "hardhat-gas-reporter";
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          }
+        },
+      },
+    ],
+  },
+};
