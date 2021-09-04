80c164655e23e4aca453b1f919fddb1584d83f13
Tony Jin <nijynot@gmail.com>
2021-09-04 13:31:57 +0200

    fix licensing

--- contracts/libraries/utils/Lock.sol
+++ contracts/libraries/utils/Lock.sol
@@ -1,7 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-
-import "hardhat/console.sol";
 
 abstract contract Lock {
     bool public unlocked = true; // unlocked at creation
