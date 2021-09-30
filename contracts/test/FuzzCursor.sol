f95ec49393978241fd3828261b288652102ba088
Tony Jin <nijynot@gmail.com>
2021-09-30 13:57:42 +0200

    add cursor and kernel fuzz

---
+++ contracts/test/FuzzCursor.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "../libraries/math/Cursor.sol";
+
+contract FuzzCursor {
+    event AssertionFailed();
+
+    function getCursorInvariant(uint256 input) public {
+        uint256 cursor = Cursor.getCursor(input, 18);
+        if ((1 << cursor + 2) - 1 <= input / 10 ** 18) emit AssertionFailed();
+        if ((1 << cursor) - 1 > input / 10 ** 18) emit AssertionFailed();
+    }
+
+    function getCursorRoundingUpInvariant(uint256 input) public {
+        uint256 cursor = Cursor.getCursorRoundingUp(input, 18);
+        if ((1 << cursor + 1) - 1 <= input / 10 ** 18) emit AssertionFailed();
+        if ((1 << cursor) - 1 > input / 10 ** 18) emit AssertionFailed();
+    }
+}
